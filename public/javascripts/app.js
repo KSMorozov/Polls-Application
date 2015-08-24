423d5e905b1ed42f2ba9c6f3a1203515c290ec0c
Kirill Morozov <tiguhrrr@gmail.com>
2015-08-24 21:25:00 +0300

    Bootstrap Angular-Material, since it has better synergy with angularJS

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,8 @@
-angular.module('PollsApp', []);
+angular.module('PollsApp', ['ngMaterial', 'ngMdIcons']);
+
+angular.module('PollsApp')
+.config(function ($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('purple')
+    .accentPalette('deep-purple');
+});
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Your Polls.' });
+  res.render('index', { title: 'Polls.' });
 });
 
 module.exports = router;
